a99b0e274c4be0cd7c8996bb6a4e06301a5cb075
Alex Berry <berry3@indiana.edu>
2016-11-01 22:38:06 -0400

    adding support for setting widths closes #1

--- src/main/java/org/libraryweasel/traytable/TrayTable.java
+++ src/main/java/org/libraryweasel/traytable/TrayTable.java
@@ -22,20 +22,26 @@ import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public final class TrayTable {
-    public static TableView<List<String>> create(List<String> headers, List<List<String>> content) {
+    public static TableView<List<String>> create(List<String> headers, List<Integer> widths, List<List<String>> content) {
         final TableView<List<String>> table = new TableView<>();
 
         IntStream.range(0, headers.size()).forEach((final int index) -> {
             final TableColumn<List<String>, String> column = new TableColumn<>(headers.get(index));
+            if (widths.size() > index) {column.setPrefWidth(widths.get(index)); }
             table.getColumns().add(column);
             column.setCellValueFactory(row ->
                 new ReadOnlyStringWrapper((index < row.getValue().size()) ? row.getValue().get(index) : ""));
         });
         table.getItems().addAll(content);
         return table;
+    }
+
+    public static TableView<List<String>> create(List<String> headers, List<List<String>> content) {
+        return create(headers, new ArrayList<>(), content);
     }
 }
